3f2945d57fd473d3663dc33de09ffc97b78fb8ed
chris <zrzllyz0@gmail.com>
2017-12-02 19:39:05 -0500

    Initial commit

---
+++ src/main/java/com/chrisspringboot/demo/webController/Controller.java
@@ -0,0 +1,13 @@
+package com.chrisspringboot.demo.webController;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @RequestMapping("/")
+    public String HomePage() {
+        return "Hello My precious Bao";
+    }
+}
